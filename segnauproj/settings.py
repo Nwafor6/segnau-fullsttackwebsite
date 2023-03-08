fa37aea6bd083712479847eebae7db8ba085d83e
Nwafor6 <nwaforglory6@gmail.com>
2023-03-09 00:34:58 +0100

    added cloud hosting to allowed host

--- segnauproj/settings.py
+++ segnauproj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(#6*dkjriqkh(&8d4yrqvhp-7z_8kb7d#)_^*_u*u58=3-yo3_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["segnau.pythonanywhere.com"]
 
 
 # Application definition
